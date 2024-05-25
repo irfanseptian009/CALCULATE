472f36dfbc5e8167bcfe68419b441affb3b00c85
irfan septian <ivanseftian79@gmai.com>
2024-05-26 00:45:59 +0700

    add button

--- javascrip.js
+++ javascrip.js
@@ -3,3 +3,24 @@ const display = document.getElementById("Display");
 function appendToDisplay(input) {
   display.value += input;
 }
+
+function clearDisplay() {
+  display.value = "";
+}
+
+function backspace() {
+  const displayValue = display.value;
+  if (!displayValue) {
+    return;
+  }
+  const newDisplayValue = displayValue.slice(0, -1);
+  display.value = newDisplayValue;
+}
+
+function calculate() {
+  try {
+    display.value = eval(display.value);
+  } catch (error) {
+    display.value = "Err";
+  }
+}
